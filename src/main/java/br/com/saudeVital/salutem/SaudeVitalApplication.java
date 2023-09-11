8379101e75681befbc6b6d22ef5aca1a48964c85
Walker de Carvalho <walker@hipcom.com.br>
2023-09-11 17:11:20 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/saudeVital/salutem/SaudeVitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.saudeVital.salutem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeVitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeVitalApplication.class, args);
+	}
+
+}
